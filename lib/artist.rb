803510be89e70288116a1b3b8fa895ec7cd5d2fa
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-09-22 20:57:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,38 +1,3 @@
 class Artist
-  attr_accessor :name
-  @@song_count = 0
 
-  def initialize(name)
-    @name = name
-    @songs = []
-
-    def add_song(song)
-      self.songs << song
-      song.artist = self
-    end
-
-  def songs
-    @songs
-  end
-
-
-def artist_name
-  @name
-end
-
-def add_song_by_name(name)
-   song = Song.new(name)
-   self.songs << song
-   song.artist = self
-   @@song_count +=1
- end
-
- def songs
-   @songs
- end
-
- def self.song_count
-   @@song_count
- end
- end
 end
